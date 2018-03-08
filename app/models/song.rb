192b58b96155ccc2c94b33bf7f9da46416ef5fe7
An Nuyen <annuyen@gmail.com>
2018-03-08 16:48:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,4 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  belongs_to :genre
-
-    def create
-    end
-
-    def update
-    end
-
-    def show
-    end
-
+  belongs_to :genre     
 end
